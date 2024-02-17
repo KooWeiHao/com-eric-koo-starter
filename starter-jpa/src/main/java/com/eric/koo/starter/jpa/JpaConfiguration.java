a5cde168abde3f69e4a3361ba0888f33ce444f2f
erickoo <ericcool.1129@gmail.com>
2024-02-17 16:49:03 +0800

    Show starter enabled info

--- starter-jpa/src/main/java/com/eric/koo/starter/jpa/JpaConfiguration.java
+++ starter-jpa/src/main/java/com/eric/koo/starter/jpa/JpaConfiguration.java
@@ -1,9 +1,18 @@
 package com.eric.koo.starter.jpa;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
+@Log4j2
 @Configuration
 @ComponentScan(basePackageClasses = JpaConfiguration.class)
 class JpaConfiguration {
+
+    @PostConstruct
+    private void init() {
+        log.info("{}", "[Starter JPA] is enabled");
+    }
 }
